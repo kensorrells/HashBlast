632028771f22a49ad49bb74f4ffb33001778cf5f
kensorrells <51687041+kensorrells@users.noreply.github.com>
2019-07-08 20:19:02 -0500

    Update hashblast.py

--- hashblast.py
+++ hashblast.py
@@ -1,32 +1,58 @@
+#python3
+
 import hashlib
 
+txt_ent = 1
+fun_type = 1
+hsh_format = 1
+run_cont = True
 
-def input_hash():
-    
-    #I created the variable md, beauase later I will be converting these hashes back to English
-
-    md = input("Encode your data: ")
-
-    
-    print(" \n             MD5                     ")
-    print("________________________________________")
-    print(hashlib.md5(md.encode()).hexdigest())
-    print("________________________________________")
-
-    print(" \n            Sha256                   ")
-    print("____________________________________________________________________")
-    print(hashlib.sha256(md.encode()).hexdigest())
-    print("____________________________________________________________________")
-
-    print(" \n            Sha512                   ")
-    print("____________________________________________________________________________________________")
-    print(hashlib.sha512(md.encode()).hexdigest())
-    print("____________________________________________________________________________________________")
-    
-
-input_hash()
-
-
-#HAPPY HASHING!!!
-
-
+while run_cont == True:
+ print("Please choose your format.")
+ hsh_format = input("Format: ")
+ print("Would you like to encrypt or decrypt?")
+ fun_type = input("Function: ")
+ print("Please enter the text.")
+ txt_ent = input("Text: ")
+ print("________________________________________")
+ if hsh_format == 'SHA1':
+  if fun_type == 'encrypt':
+   print(hashlib.sha1(txt_ent.encode()).hexdigest())
+  else:
+   print("")
+ elif hsh_format == 'SHA224':
+  if fun_type == 'encrypt':
+   print(hashlib.sha224(txt_ent.encode()).hexdigest())
+  else:
+   print("")
+ elif hsh_format == 'SHA256':
+  if fun_type == 'encrypt':
+   print(hashlib.sha256(txt_ent.encode()).hexdigest())
+  else:
+   print("")
+ elif hsh_format == 'SHA384':
+  if fun_type == 'encrypt':
+   print(hashlib.sha384(txt_ent.encode()).hexdigest())
+  else:
+   print("")
+ elif hsh_format == 'SHA512':
+  if fun_type == 'encrypt':
+   print(hashlib.sha512(txt_ent.encode()).hexdigest())
+  else:
+   print("")
+ elif hsh_format == 'MD5':
+  if fun_type == 'encrypt':
+   print(hashlib.md5(txt_ent.encode()).hexdigest())
+  else:
+   print("")
+ else:
+     print("ERROR")
+     BREAK
+ print("________________________________________")
+ print("Would you like to do anything else?")
+ cont = input("   : ")
+ cont.lower
+ if cont != 'no' or 'n':
+     run_cont = False
+ else:
+     run_cont = True

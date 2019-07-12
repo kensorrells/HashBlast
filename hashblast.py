936c41bfc3d5d1a1c434d92a51c4ce86f7285c27
kensorrells <51687041+kensorrells@users.noreply.github.com>
2019-07-12 13:39:22 -0500

    Combines encryptor and decryptor

--- hashblast.py
+++ hashblast.py
@@ -9,7 +9,11 @@ run_cont = True
 run_turns = 0
 chr_num = 0
 cur_chr = 0
-
+msg_aft = ''
+x=0
+i=0
+a=0
+cant = ''
 while run_cont == True:
  print("Please choose your format.")
  hsh_format = input("Format: ")
@@ -18,46 +22,58 @@ while run_cont == True:
  print("Please enter the text.")
  txt_ent = input("Text: ")
  chr_num = len(txt_ent)
- print("What is the first encryption number?")
- encrpt_1 = input(": ")
  print("_______________________________________________")
  while run_turns +1 <= chr_num:
   cur_chr = txt_ent[run_turns]
-  cur_chr += encrpt_1
-  if hsh_format == 'SHA1':
-   if fun_type == 'encrypt':
-    print(hashlib.sha1(cur_chr.encode()).hexdigest())
+  if fun_type == 'encrypt':
+   txtsha1 = hashlib.sha1(cur_chr.encode()).hexdigest()
+   txtsha224 = hashlib.sha224(cur_chr.encode()).hexdigest()
+   txtsha256 = hashlib.sha256(cur_chr.encode()).hexdigest()
+   txtsha384 = hashlib.sha384(cur_chr.encode()).hexdigest()
+   txtsha512 = hashlib.sha512(cur_chr.encode()).hexdigest()
+   txtmd5 = hashlib.md5(cur_chr.encode()).hexdigest()
+   if hsh_format == 'sha1':
+    msg_aft += txtsha1
+   elif hsh_format == 'sha224':
+    msg_aft += txtsha224
+   elif hsh_format == 'sha256':
+    msg_aft += txtsha256
+   elif hsh_format == 'sha384':
+    msg_aft += txtsha384
+   elif hsh_format == 'sha512':
+    msg_aft += txtsha512
    else:
-    print("")
-  elif hsh_format == 'SHA224':
-   if fun_type == 'encrypt':
-    print(hashlib.sha224(cur_chr.encode()).hexdigest())
-   else:
-    print("")
-  elif hsh_format == 'SHA256':
-   if fun_type == 'encrypt':
-    print(hashlib.sha256(cur_chr.encode()).hexdigest())
-   else:
-    print("")
-  elif hsh_format == 'SHA384':
-    if fun_type == 'encrypt':
-      print(hashlib.sha384(cur_chr.encode()).hexdigest())
-    else:
-      print("")
-  elif hsh_format == 'SHA512':
-    if fun_type == 'encrypt':
-      print(hashlib.sha512(cur_chr.encode()).hexdigest())
-    else:
-      print("")
-  elif hsh_format == 'MD5':
-    if fun_type == 'encrypt':
-      print(hashlib.md5(cur_chr.encode()).hexdigest())
-    else:
-      print("")
+    msg_aft += txtmd5
+    run_turns += 1
   else:
-    print("ERROR:")
-    break
+    chrlist = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+           'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+           ' ','1','2','3','4','5','6','7','8','9','0']
+    fnl_msg = ''
+    cent = txt_ent
+    if hsh_format == 'sha1':
+      while x <= len(cent):
+        while i+x <= x+39:
+         y = x + i
+         cant += cent[y]
+         print(cant)
+         i += 1
+        i = 0
+        while a <= 62:
+          b = chrlist[a]
+          b =  hashlib.sha1(b.encode()).hexdigest()
+          if cant == b:
+            fnl_msg += chrlist[a]
+            print(fnl_msg)
+            a = 100
+          else:
+            a += 1
+        a = 0
+        cant = ''
+        x += 40
+        print(fnl_msg)
   run_turns += 1
+ print(msg_aft)
  print("_______________________________________________")
  print("Would you like to do anything else?")
  cont = input("   : ")
